ca749743a4b8e6f97a73e3d493577e2d58032ebf
AlinaGladkova <tina.leaf89@gmail.com>
2024-08-15 15:02:04 +0300

    first commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,6 @@
+import Counter from "../counter/Counter";
+
+function App() {
+  return <Counter title={"Super Counter"} initialNum={5} />;
+}
+export default App;
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import styles from "./counter.module.css";
+
+//менять цвета цифр
+//увеличивать/уменьшать на разное кол-во
+
+// Вопросы:
+// - как обращаться к какому-либо элементу
+// - как менять стили
+// - как вызывать несколько методов для одного элемента (или в листенере)
+// - как записывать сложные стейты
+
+function Counter({ title, initialNum }) {
+  const [count, setCount] = useState(initialNum);
+  const currentColor = count === 0 ? styles["counter__result--zero"] : count > 0 ? styles["counter__result--positive"] : styles["counter__result--negative"];
+
+  const minusLimit = () => {
+    // if (count === 0) {
+    //   return;
+    // }
+    setCount(count - 1);
+  };
+
+  return (
+    <div className={styles.counter}>
+      <h2 className={styles.counter__title}>{title}</h2>
+      <span className={`${styles.counter__result} ${currentColor}`}>{count}</span>
+      <div className={styles.counter__edit}>
+        <button className="btn btn--minus" onClick={minusLimit}>
+          -
+        </button>
+        <button className="btn btn--reset" onClick={() => setCount(initialNum)}>
+          reset
+        </button>
+        <button className="btn btn--plus" onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
